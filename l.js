e6943bf3d931a98216d1bb6224c0057e52b1f52b
attirR10 <atirmidzirizkyasantosa@gmail.com>
2026-07-04 12:25:29 +0700

    Add files via upload
    kalkulator manual

---
+++ l.js
@@ -0,0 +1,15 @@
+const NumBa = 100;
+const NumBa1 = 100;
+const result = ":";
+
+if (result === "+") {
+  console.log(NumBa + NumBa1);
+} else if (result === "-") {
+  console.log(NumBa - NumBa1);
+} else if (result === "x") {
+  console.log(NumBa * NumBa1);
+} else if (result === ":") {
+  console.log(NumBa / NumBa1);
+} else {
+  console.log("not valid");
+}
